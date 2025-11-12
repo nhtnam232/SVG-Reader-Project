764b0f7029cea4894a40ec13aa4f6501ed477f09
eldye24 <nguyenhoangnam2006tq@gmail.com>
2025-11-12 11:01:14 +0700

    update Circle.cpp & Rect.cpp

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -1,8 +1,16 @@
 #include"Circle.h"
 
 void Circle::parse(XMLElement* node) {
-
+	FilledShape::parse(node);
+	node->QueryFloatAttribute("cx", &m_cx);
+	node->QueryFloatAttribute("cy", &m_cy);
+	node->QueryFloatAttribute("r", &m_r);
 }
 void Circle::draw(sf::RenderWindow &window) {
-
+	sf::CircleShape circle(m_r);
+	circle.setPosition(m_cx - m_r, m_cy - m_r);
+	circle.setFillColor(m_fill.getColor());
+	circle.setOutlineColor(m_stroke.getColor());
+	circle.setOutlineThickness(m_stroke_width);
+	window.draw(circle);
 }
--- src/Rect.cpp
+++ src/Rect.cpp
@@ -1,8 +1,17 @@
 #include"Rect.h"
 
 void Rect::parse(XMLElement* node) {
-
+	FilledShape::parse(node);
+	node->QueryFloatAttribute("x", &m_x);
+	node->QueryFloatAttribute("y", &m_y);
+	node->QueryFloatAttribute("width", &m_width);
+	node->QueryFloatAttribute("height", &m_height);
 }
 void Rect::draw(sf::RenderWindow& window) {
-
+	sf::RectangleShape rectangle(sf::Vector2f(m_width, m_height));
+	rectangle.setPosition(m_x, m_y);
+	rectangle.setFillColor(m_fill.getColor());
+	rectangle.setOutlineColor(m_stroke.getColor());
+	rectangle.setOutlineThickness(m_stroke_width);
+	window.draw(rectangle);
 }
