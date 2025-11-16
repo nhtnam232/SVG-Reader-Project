323fd319a1fb5bc2dc04badff60bbcc440f884dc
eldye24 <nguyenhoangnam2006tq@gmail.com>
2025-11-17 00:08:10 +0700

    update Text

--- src/Text.cpp
+++ src/Text.cpp
@@ -21,7 +21,12 @@ void myText::draw(Gdiplus::Graphics& g) {
 	fill.setOpacity(m_fill_opacity);
 	Gdiplus::SolidBrush brush(fill.getColor());
 
-	Gdiplus::PointF pos(m_x, m_y);
+	Gdiplus::FontFamily fontFamily(fontName.c_str());
+	Gdiplus::REAL ascent = fontFamily.GetCellAscent(Gdiplus::FontStyleRegular);
+	Gdiplus::REAL emHeight = fontFamily.GetEmHeight(Gdiplus::FontStyleRegular);
+	Gdiplus::REAL baselineOffset = m_font_size * ascent / emHeight;
+
+	Gdiplus::PointF pos(m_x, m_y - baselineOffset);
 
 	g.DrawString(wstring(m_text.begin(), m_text.end()).c_str(), -1, &font, pos, &brush);
 }
