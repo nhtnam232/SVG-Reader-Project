b69093c994cb5272e656a0347c2df9ed4a9896dd
NGUYỄN NHÂN KIỆT <dinanocs>
2025-12-13 19:03:22 +0700

    Viết hàm draw chỉnh transform qua matrix cho Circle, Ellipse, Rect, Text

--- src/Rect.cpp
+++ src/Rect.cpp
@@ -1,4 +1,4 @@
-#include"Rect.h"
+﻿#include"Rect.h"
 void myRect::parse(tinyxml2::XMLElement* node) {
 	myFilledShape::parse(node);
 	node->QueryFloatAttribute("x", &m_x);
@@ -7,14 +7,36 @@ void myRect::parse(tinyxml2::XMLElement* node) {
 	node->QueryFloatAttribute("height", &m_height);
 }
 
-void myRect::draw(Gdiplus::Graphics& g) {
+void myRect::draw(Gdiplus::Graphics& g)
+{
+    // 1. Lưu transform gốc
+    Gdiplus::Matrix originalMatrix;
+    g.GetTransform(&originalMatrix);
+
+    // 2. Áp transform của rect
+    Gdiplus::Matrix* transformMatrix = m_transforms.getFinalMatrix();
+    if (transformMatrix != nullptr)
+        g.MultiplyTransform(transformMatrix);
+
+    // 3. Fill
     Color fill = m_fill;
     fill.setOpacity(m_fill_opacity);
     Gdiplus::SolidBrush brush(fill.getColor());
     g.FillRectangle(&brush, m_x, m_y, m_width, m_height);
 
-    Color stroke = m_stroke;
-    stroke.setOpacity(m_stroke_opacity);
-    Gdiplus::Pen pen(stroke.getColor(), m_stroke_width);
-    g.DrawRectangle(&pen, m_x, m_y, m_width, m_height);
+    // 4. Stroke (chỉ vẽ khi hợp lệ)
+    if (m_stroke.getColor().GetAlpha() != 0 && m_stroke_width != 0)
+    {
+        Color stroke = m_stroke;
+        stroke.setOpacity(m_stroke_opacity);
+        Gdiplus::Pen pen(stroke.getColor(), m_stroke_width);
+        g.DrawRectangle(&pen, m_x, m_y, m_width, m_height);
+    }
+
+    // 5. Restore transform
+    g.SetTransform(&originalMatrix);
+
+    // 6. Giải phóng matrix
+    if (transformMatrix != nullptr)
+        delete transformMatrix;
 }
--- src/Text.cpp
+++ src/Text.cpp
@@ -1,4 +1,4 @@
-#include"Text.h"
+﻿#include"Text.h"
 void myText::parse(tinyxml2::XMLElement* node) {
 	myFilledShape::parse(node);
 	node->QueryFloatAttribute("x", &m_x);
@@ -10,23 +10,50 @@ void myText::parse(tinyxml2::XMLElement* node) {
 	}
 }
 
-void myText::draw(Gdiplus::Graphics& g) {
-	if (m_text.empty()) {
-		return;
-	}
-	std::wstring fontName = L"Times New Roman";
-	Gdiplus::Font font(fontName.c_str(), m_font_size, Gdiplus::FontStyleRegular, Gdiplus::UnitPixel);
+void myText::draw(Gdiplus::Graphics& g)
+{
+    if (m_text.empty())
+        return;
 
-	Color fill = m_fill;
-	fill.setOpacity(m_fill_opacity);
-	Gdiplus::SolidBrush brush(fill.getColor());
+    // 1. Lưu transform gốc
+    Gdiplus::Matrix originalMatrix;
+    g.GetTransform(&originalMatrix);
 
-	Gdiplus::FontFamily fontFamily(fontName.c_str());
-	Gdiplus::REAL ascent = fontFamily.GetCellAscent(Gdiplus::FontStyleRegular);
-	Gdiplus::REAL emHeight = fontFamily.GetEmHeight(Gdiplus::FontStyleRegular);
-	Gdiplus::REAL baselineOffset = m_font_size * ascent / emHeight;
+    // 2. Áp transform của text
+    Gdiplus::Matrix* transformMatrix = m_transforms.getFinalMatrix();
+    if (transformMatrix != nullptr)
+        g.MultiplyTransform(transformMatrix);
 
-	Gdiplus::PointF pos(m_x, m_y - baselineOffset);
+    // 3. Font
+    std::wstring fontName = L"Times New Roman";
+    Gdiplus::Font font(
+        fontName.c_str(),
+        m_font_size,
+        Gdiplus::FontStyleRegular,
+        Gdiplus::UnitPixel
+    );
 
-	g.DrawString(wstring(m_text.begin(), m_text.end()).c_str(), -1, &font, pos, &brush);
+    // 4. Fill
+    Color fill = m_fill;
+    fill.setOpacity(m_fill_opacity);
+    Gdiplus::SolidBrush brush(fill.getColor());
+
+    // 5. Tính baseline (SVG y là baseline)
+    Gdiplus::FontFamily fontFamily(fontName.c_str());
+    Gdiplus::REAL ascent = fontFamily.GetCellAscent(Gdiplus::FontStyleRegular);
+    Gdiplus::REAL emHeight = fontFamily.GetEmHeight(Gdiplus::FontStyleRegular);
+    Gdiplus::REAL baselineOffset = m_font_size * ascent / emHeight;
+
+    Gdiplus::PointF pos(m_x, m_y - baselineOffset);
+
+    // 6. Vẽ text
+    std::wstring wtext(m_text.begin(), m_text.end());
+    g.DrawString(wtext.c_str(), -1, &font, pos, &brush);
+
+    // 7. Restore transform
+    g.SetTransform(&originalMatrix);
+
+    // 8. Giải phóng matrix
+    if (transformMatrix != nullptr)
+        delete transformMatrix;
 }
